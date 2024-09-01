4d18dd1c0e95e58eb22bd70fac217455931a1e89
Nica Zoe <nica.dable@gmail.com>
2024-09-01 22:54:29 +0800

    .

--- aboutme/src/components/Footer/Footer.jsx
+++ aboutme/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
 
 export const Footer = () => {
   return (
